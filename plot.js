3f1bca35662ff8c05343dc7d1ba386d05a3f82af
NJ <nj@Nirmals-MBP.zyxel.com>
2020-03-21 15:41:15 -0400

    NJ - Refactored the code removed files that were not used

--- plot.js
+++ plot.js
@@ -5,18 +5,75 @@ function populateDemographicInfo(givenSample) {
 
   demographicInfo.html("");
 
-  //var filteredDate = tableData.filter(record => record.datetime === inputDateValue);
-
   var keys = d3.keys(givenSample);
   var values = d3.values(givenSample);
 
-  console.log(keys.length);
-
   for (var i = 0; i < keys.length; i++) {
-    demographicInfo.append("h5").text(`${keys[i]}:${values[i]}`)
+    demographicInfo.append("h6").text(`${keys[i]}:${values[i]}`)
+  }
+}
+
+//This function will take the sorted data
+//will do the necessary data clean up and plot the bar chart
+function plotBarChart(sortedData) {
+
+  //Pick the OTU ID take the top 10 order it ascending and convert to a string
+  var otuIds = sortedData[0].otu_ids.slice(0, 10).reverse().map(String);
+  //Once a string append OTU to the beginning
+  for (var i = 0; i < otuIds.length; i++) {
+    otuIds[i]="OTU "+otuIds[i];
   }
 
+  //Pick the OTU Values and OTU labels take the top 10 order it ascending
+  var otuIdValues = sortedData[0].sample_values.slice(0, 10).reverse();
+  var otuLabels = sortedData[0].otu_labels.slice(0, 10).reverse();
+
+  // Trace1 for the horizontal bar chart
+  var data = [{
+    type: "bar",
+    x: otuIdValues,
+    y: otuIds,
+    text: otuLabels,
+    orientation: "h",
+    mode: "markers",
+    marker: {
+      colorscale: "Earth",
+      width: 1
+    },
+  }];
+
+  // Apply the group bar mode to the layout
+  var layout = {
+    title: "Top 10 OTUs"
+  };
+
+  Plotly.newPlot("bar", data, layout);
 }
+
+//This function will take the filtered data and plot the bubble chart
+function plotBubbleChart(filteredData) {
+
+  var bubbleTrace = {
+    x: filteredData[0].otu_ids,
+    y: filteredData[0].sample_values,
+    text: filteredData[0].otu_labels,
+    mode: "markers",
+    marker: {
+      colorscale: "RdBu",
+      size: filteredData[0].sample_values
+    }
+  };
+
+  var bubbleData = [bubbleTrace];
+
+  var bubbleLayout = {
+    title: "OTU Bubble Chart",
+    xaxis: { title: "OTU ID"}
+  };
+
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+}
+
 function buildPlot() {
 
   // Grab a reference to the dropdown select element
@@ -36,59 +93,32 @@ function buildPlot() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames.metadata[0];
-    console.log(firstSample);
     populateDemographicInfo(firstSample);
+
+    //Get the OTU names and values
+    var filteredSample = [sampleNames.samples[0]];
+    plotBarChart(filteredSample);
+    plotBubbleChart(filteredSample);
 
   });
 }
 
 function optionChanged(newSample) {
-	     console.log(newSample);
+
        //read the json file
        d3.json("data/samples.json").then((sampleNames) => {
          //Get the ID to populate the demographic info
          var filteredMetaData = sampleNames.metadata.filter(record => record.id == newSample);
          populateDemographicInfo(filteredMetaData[0]);
 
-         //Get the OTU names and values filter it and then sort it
+         //console.log(filteredMetaData[0].wfreq);
+
+         //Get the OTU names and values
          var filteredSample = sampleNames.samples.filter(record => record.id == newSample);
          var sortedByOTUValues = filteredSample.sort((a, b) => b.sample_values - a.sample_values);
-         console.log(sortedByOTUValues);
-         var otuIds = sortedByOTUValues[0].otu_ids.slice(0, 10).reverse().map(String);
-         for (var i = 0; i < otuIds.length; i++) {
-           otuIds[i]="OTU "+otuIds[i];
-         }
-         var otuIdValues = sortedByOTUValues[0].sample_values.slice(0, 10).reverse();
-         var otuLabels = sortedByOTUValues[0].otu_labels.slice(0, 10).reverse();
 
-         console.log(otuIds);
-
-         // Trace1 for the horizontal bar chart
-         var data = [{
-           type: "bar",
-           x: otuIdValues,
-           y: otuIds,
-           text: otuLabels,
-           orientation: "h",
-           mode: "markers",
-           marker: {
-             colorscale: "Earth",
-             width: 1
-           },
-         }];
-
-         // Apply the group bar mode to the layout
-         var layout = {
-           title: "Top 10 OTUs"
-
-         };
-
-         Plotly.newPlot("bar", data, layout);
-
-
-         // Slice the first 10 objects for plotting
-         //slicedOTUData = sortedByOTUValues.slice(0, 10);
-
+         plotBarChart(sortedByOTUValues);
+         plotBubbleChart(filteredSample);
 
        })
     };
